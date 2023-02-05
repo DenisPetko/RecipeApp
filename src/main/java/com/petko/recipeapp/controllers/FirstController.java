d8c6d48b7ff4ca97df2859082191f955235648a4
denispetko <den.petko@rambler.ru>
2023-02-05 21:11:03 +0300

    init

---
+++ src/main/java/com/petko/recipeapp/controllers/FirstController.java
@@ -0,0 +1,23 @@
+package com.petko.recipeapp.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class FirstController {
+
+    @GetMapping("/")
+    public String print() {
+        return "Приложение запущено";
+    }
+
+    @GetMapping("/info")
+    public String printInfo() {
+        return """
+                Имя ученика: Денис;
+                Название проекта: RecipeApp;
+                Дата создания: 05.02.2023;
+                описание проекта в свободной форме""";
+    }
+}
